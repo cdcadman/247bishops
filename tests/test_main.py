389c62d61f6a252161a185fe97ad30cb379e9152
Chuck Cadman <51368516+cdcadman@users.noreply.github.com>
2024-11-10 19:00:32 -0800

    Set response headers to enforce security policies

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import requests
 
+from webapp_python.main import RESPONSE_HEADERS
+
 from .server import get_server_url
 
 
@@ -7,9 +9,18 @@ def test_main():
     with get_server_url() as webapp_url:
         response = requests.get(f"{webapp_url}", timeout=60)
         assert response.status_code == 200
+        for key, val in RESPONSE_HEADERS.items():
+            assert response.headers[key] == val
         assert "24/7 Bishops" in response.text
         response = requests.get(f"{webapp_url}/favicon.svg?v=1729", timeout=60)
         assert response.status_code == 200
+        for key, val in RESPONSE_HEADERS.items():
+            assert response.headers[key] == val
+        response = requests.get(f"{webapp_url}/css/main.css", timeout=60)
+        assert response.status_code == 200
+        for key, val in RESPONSE_HEADERS.items():
+            assert response.headers[key] == val
+        assert "background-color:green" in response.text
 
 
 def test_maint():
@@ -17,6 +28,10 @@ def test_maint():
         response = requests.get(f"{webapp_url}", timeout=60)
         assert response.status_code == 503
         assert "24/7 Bishops is Temporarily Unavailable" in response.text
+        for key, val in RESPONSE_HEADERS.items():
+            assert response.headers[key] == val
         response = requests.post(f"{webapp_url}/random_path", timeout=60)
         assert response.status_code == 503
         assert "24/7 Bishops is Temporarily Unavailable" in response.text
+        for key, val in RESPONSE_HEADERS.items():
+            assert response.headers[key] == val
