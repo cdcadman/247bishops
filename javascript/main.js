940b4fe567c52d6fd8ab6498a496068cd51f03b4
Chuck Cadman <51368516+cdcadman@users.noreply.github.com>
2024-11-20 08:56:40 -0800

    Add menu item for work board.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,21 @@
+function current_page() {
+    for (const div of document.getElementsByClassName("page")) {
+        if (!div.classList.contains("hidden")) {
+            return div.id;
+        }
+    }
+}
+
+function switch_to(page_id) {
+    cpage = current_page();
+    document.getElementById(cpage).classList.add("hidden");
+    if (cpage == page_id) {
+        document.getElementById("landing_content").classList.remove("hidden");
+    } else {
+        document.getElementById(page_id).classList.remove("hidden");
+    }
+}
+
 document.getElementById("about").addEventListener('click',
     () => {
         document.getElementById("about_popup").classList.add("show");
@@ -10,3 +28,8 @@ document.getElementById("close_about_popup").addEventListener('click',
         document.getElementById("content").classList.remove("blur");
     }
 );
+document.getElementById("work_board").addEventListener('click',
+    () => {
+        switch_to("work_board_content");
+    }
+)
